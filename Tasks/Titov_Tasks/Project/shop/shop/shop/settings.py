2a9534fc38206eeb79c2845e64bae921d1f7761b
titov-evgeni <t1t0v@tut.by>
2021-06-05 23:32:51 +0300

    “os_key”

--- Tasks/Titov_Tasks/Project/shop/shop/shop/settings.py
+++ Tasks/Titov_Tasks/Project/shop/shop/shop/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-!&1=1=_xc+$r(hbi3^n*ptpg*c5^s3s!6_8kl1_0n&e+3javvb'
+SECRET_KEY = os.environ.get('PROJECT_SECRET_KEY', 'django-insecure-!&1=1=_xc+$r(hbi3^n*ptpg*c5^s3s!6_8kl1_0n&e+3javvb')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
